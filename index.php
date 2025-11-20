2dc0150bc669680d7cce5540c24aa1578250ff21
Anderson Casimiro <casimiro.arruda@gmail.com>
2025-11-20 06:17:50 -0300

    feat: Add CORS handling and return initial tasks as JSON.

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php echo "API em Construção";
+<?php
+# CORS
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Accept');
+# tratamento para o request OPTIONS do Navegador
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+header('Content-Type: application/json');
+# Nossas tarefas iniciais
+$tasks = [
+    ['id' => 1, 'title' => 'Conferir o setup', 'status' => 'Concluída', 'starred' => true],
+    ['id' => 2, 'title' => 'Resolver o CORS', 'status' => 'Pendente', 'starred' => false]
+];
+
+echo json_encode($tasks);
